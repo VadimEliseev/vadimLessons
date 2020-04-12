66a61acc786a60ccc0af07b0b81e89d1be6585b7
vadim <kirpichni@ya.ru>
2020-04-12 11:07:51 +0300

    Сделал пока два

--- src/main/java/tasks/arrays/BubbleSort.java
+++ src/main/java/tasks/arrays/BubbleSort.java
@@ -1,8 +1,29 @@
 package tasks.arrays;
+import java.util.Arrays;
 
 public class BubbleSort {
-    public static void bubbleSort(int[] array){
-        //нужно отсортировать этот массив по возрастанию методом "пузырька"
-        //https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0_%D0%BF%D1%83%D0%B7%D1%8B%D1%80%D1%8C%D0%BA%D0%BE%D0%BC
+    public static void bubbleSort(int[] array) {
+        //Arrays.sort(array);   //Если я правильно понял данный стандартный метод делает все согластно заданию
+        //System.out.println(Arrays.toString(array));
+        //}
+        System.out.print("Input Array :");
+        System.out.println(Arrays.toString(array));
+        int k=0;
+        while (k<array.length-1) {
+            for (int n = 0; n < array.length - 1; n++) {
+                if (array[n] > array[n + 1]) {
+                    int a = array[n + 1];
+                    array[n + 1] = array[n];
+                    array[n] = a;
+                }
+            }
+            k++;
+        }
+        System.out.print("Output Array :");
+        System.out.println(Arrays.toString(array));
+    }
+    public static void main(String[] args) {
+        int[] myArray = {1,45,1,7,3,5,2,9};
+        bubbleSort(myArray);
     }
 }
