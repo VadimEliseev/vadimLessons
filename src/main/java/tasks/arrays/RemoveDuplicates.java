d7c09d377891a2f2c30b967c6cd7dfbf904e13aa
vadim <kirpichni@ya.ru>
2020-04-13 15:08:02 +0300

    finished

--- src/main/java/tasks/arrays/RemoveDuplicates.java
+++ src/main/java/tasks/arrays/RemoveDuplicates.java
@@ -1,12 +1,48 @@
 package tasks.arrays;
 
+import java.util.Arrays;
+
 public class RemoveDuplicates {
-    public static String[] removeDuplicates(String[] array){
+    public static String[] removeDuplicates(String[] array) {
         //метод должен возвращать новый массив в котором все повторно встречающиеся элементы удалены
         //например {"a", "b", "a", "b", "c", "c"} становится {"a", "b", "c"}
         //в остальном порядок меняться не должен, просто убрать повторные
         //имей ввиду что получается - ты не знаешь изначально какой длины будет массив с ответом
 
-        return null;
+        for (int k = 0; k< array.length-1; k++) {
+
+            for (int a = 1; a < array.length; a++) {
+                if (k < a) {
+                    if (array[k] == array[a]) {
+                        array[a] = null;
+                    }
+                }
+            }
+        }
+        int r=0;
+        for (String t : array) {
+            if (t == null) {
+            }
+            else{
+                r++;
+            }
+        }
+
+        String[] array2 = new String[r];
+        int t = 0;
+        for (int a =0; a < array.length-1; a++) {
+            if (array[a] == null) {
+            } else {
+                array2[t] = array[a];
+                    t++;
+            }
+        }
+        return array2;
+    }
+
+    public static void main(String[] args) {
+        String[] myarray = {"a","b","c","a","d","c","b","r","a","f","f"};
+        System.out.println("Output Array :"+(Arrays.toString(removeDuplicates(myarray))));
     }
 }
+
