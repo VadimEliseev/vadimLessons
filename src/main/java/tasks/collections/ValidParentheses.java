459eb498807ce569be0cc3cf5e324968bd046c0b
vadim <kirpichni@ya.ru>
2020-04-22 21:30:16 +0300

    Revert "ValidParentheses task"
    This reverts commit ba3109a0

--- src/main/java/tasks/collections/ValidParentheses.java
+++ src/main/java/tasks/collections/ValidParentheses.java
@@ -1,7 +1,14 @@
 package tasks.collections;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
+
 public class ValidParentheses {
-    public static boolean checkValidness(String combination) {
+
+    private static String inBrecket; // переменная для добавления новых скобок
+
+    public static boolean checkValidness(String strA)  {
         // довольно классическая задача
         // написать функцию, проверяющую правильность комбинации скобок
         // на вход подается комбинация из обычных, квадратных или фигурных скобок например "[()[{}]]"
@@ -20,7 +27,62 @@ public class ValidParentheses {
 
         // (*) Сделать так, чтобы добавить новый вид скобок можно было максимально просто и понятно.
 
+        // создание двух LinkedList
+        InputData input = new InputData();
+        LinkedList bracket = input.madeLinkedListNew(strA, inBrecket); // List из введеных скобок
+        LinkedList pairBracket = input.madeLinkedListNew(input.getListData(), inBrecket); // List из валидных пар :{}[]()
 
+
+            ValidParentheses resaultBracketList = new ValidParentheses();
+            while (resaultBracketList.equalsTwoLinked(bracket, pairBracket)) {  // сравнение-удаления двух List
+                resaultBracketList.removeBracket(bracket, pairBracket);
+            }
+
+        return bracket.size() == 0;
+
+    }
+
+    private boolean equalsTwoLinked (LinkedList linkA, LinkedList linkB){  // метод сравнения двух списков
+
+        for (int k = 0; k < linkB.size() - 1; k = k + 2) {
+            for (  int i = 0; i < linkA.size() - 1; i++) {
+                if (linkA.get(i) == linkB.get(k) && linkA.get(i + 1) == linkB.get(k + 1)) {
+                    return true;
+                }
+            }
+        }
         return false;
+    }
+     private LinkedList removeBracket(LinkedList linkA, LinkedList linkB) {  // метод удаления валидных пар
+
+        for (int k = 0; k < linkB.size() - 1; k = k+2) {  //  хотел вмосто этой части вставить метод equalsTwoLinked
+            for (int i = 0; i < linkA.size() - 1; i++) {  // но что-то пока не получилось
+                if (linkA.get(i) == linkB.get(k) && linkA.get(i + 1) == linkB.get(k + 1)) {
+                    linkA.remove(i + 1);
+                    linkA.remove(i);
+                }
+            }
+        }
+        return linkA;
+    }
+
+    public static void main(String[] args) {
+        //double d = 23.4;
+        //System.out.println(d);
+        System.out.println("Можете добавить новые пары скобок  и нажмите ENTER :");
+        System.out.println(" или просто нажмите ENTER если просто хотите продолжить: ");
+        System.out.println(" ");
+
+        Scanner scan = new Scanner(System.in);
+        inBrecket = scan.nextLine();
+
+
+        System.out.println("Введите любое колличество скобок и нажмите ENTER :");
+        System.out.println(" ");
+        String scanList = scan.nextLine();
+        System.out.println("");
+
+
+        System.out.println(checkValidness(scanList));
     }
 }
